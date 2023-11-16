624c1c3c1a4eb1fe5973a0917ffc11358ab5918d
Anas <anasahmadcs7@gmail.com>
2023-11-17 00:09:35 +0500

    Adjusted theme color

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material"
+
+export default function TestComponent(){
+    return(
+        <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+        >Hello</Button>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#14B8A6",
+    },
+    secondary:{
+      main: "#000000"
+    }
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
